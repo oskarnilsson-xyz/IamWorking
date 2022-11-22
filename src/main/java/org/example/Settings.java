369b761bb48e5e969adba32d735ce99741a3e5ca
00Lucia00 <114389235+00Lucia00@users.noreply.github.com>
2022-11-22 09:12:36 +0100

    bakgrundfärg
    lekte lite med olika funktioner och val
    så som bakgrundfärg val och fil val

--- src/main/java/org/example/Settings.java
+++ src/main/java/org/example/Settings.java
@@ -1,24 +1,26 @@
 package org.example;
 
 import javax.swing.*;
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.awt.event.*;
 import java.awt.*;
 
 
-public class Settings extends JFrame {
+public class Settings extends JFrame implements ActionListener{
     private JPanel mainPanel;
     private JPanel center;
     private JPanel bottom;
     private JPanel topp;
-    private JCheckBox checkBox1;
     private JCheckBox checkBox2;
     private JCheckBox checkBox3;
     private JCheckBox checkBox4;
-    private JCheckBox checkBox5;
-    private JCheckBox checkBox6;
-    public JButton goBack;
+    private JButton goBack;
     private JLabel label1;
+    private JButton pinkButton;
+    private JButton blackButton;
+    private JButton whiteButton;
+    private JComboBox fileSelect;
+
+    //String[] comboOptions = {"file1", "file2", "file3"};
 
     ImageIcon foxImage = new ImageIcon("fox.png"); // lade till en icon till våra fönster
 
@@ -26,23 +28,48 @@ public class Settings extends JFrame {
         initializer();// method som innehåller saker vi vill ha till våra fönster
         setSize(300,300); // specifikt för detta fönster
 
+        goBack.addActionListener(this); //istället för detta går de att använda en lambda expprestion(e -> "de som ska göras")
+        pinkButton.addActionListener(this);
+        blackButton.addActionListener(this);
+        whiteButton.addActionListener(this);
 
-        goBack.addActionListener(new ActionListener() { // gör så att tillbaka knappen tar en tillbaka till start sida
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(e.getSource().equals(goBack)){
-                    Splashpage backToSplash = new Splashpage(); // öppnar splachpage
-                    dispose();//stänger ner Settingsfönstret
-                };
-            }
-        });
     }
-    public void setColor(JButton colorSelect){// tänkte testa ändra bagrundsfärg med knaptryck, något jag kanske utvecklar senare
 
-        mainPanel.setBackground(Color.pink);
-        topp.setBackground(Color.pink);
-        center.setBackground(Color.pink);
-        bottom.setBackground(Color.pink);
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource().equals(goBack)){ // gör så att tillbaka knappen tar en tillbaka till start sida
+            Splashpage backToSplash = new Splashpage(); // öppnar splachpage
+            dispose();//stänger ner Settingsfönstret
+        }
+        if(e.getSource().equals(pinkButton)){
+            setColor(e);
+        } else if (e.getSource().equals(blackButton)) {
+            setColor(e);
+        } else if (e.getSource().equals(whiteButton)) {
+            setColor(e);
+        }
+    }
+    public void setColor(ActionEvent colorSelect){// tänkte testa ändra bagrundsfärg med knaptryck, något jag kanske utvecklar senare
+
+        if(colorSelect.getSource().equals(pinkButton)){
+            mainPanel.setBackground(Color.pink);
+            topp.setBackground(Color.pink);
+            center.setBackground(Color.pink);
+            bottom.setBackground(Color.pink);
+        }
+        if(colorSelect.getSource().equals(blackButton)){
+            mainPanel.setBackground(Color.BLACK);
+            topp.setBackground(Color.BLACK);
+            center.setBackground(Color.BLACK);
+            bottom.setBackground(Color.BLACK);
+        }
+        if(colorSelect.getSource().equals(whiteButton)){
+            mainPanel.setBackground(Color.white);
+            topp.setBackground(Color.white);
+            center.setBackground(Color.white);
+            bottom.setBackground(Color.white);
+        }
+
     }
 
     private void initializer() {// basic fakta för fönster
@@ -51,6 +78,7 @@ public class Settings extends JFrame {
         setTitle("I am working");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+
     }
 
 }
