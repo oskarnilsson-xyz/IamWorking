6a7ca7db5a3fbdc926659f47d8da3f04e6781202
00Lucia00 <114389235+00Lucia00@users.noreply.github.com>
2022-11-24 12:14:11 +0100

    Settings now effects TextInterface Window
    nu går de att ändra bakgrund och text färg på TextInterface Window

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         //var TextBasedWindow = new TextBasedWindow();
         var splashPageWindow = new Splashpage();
-        Settings settingsWindow = new Settings();
 
     }
 }
--- src/main/java/org/example/Settings.java
+++ src/main/java/org/example/Settings.java
@@ -13,6 +13,7 @@ public class Settings extends JFrame implements ActionListener,FileReadWrite{
     private JPanel bottom;
     private JPanel topp;
     private JButton goBack;
+
     private JLabel label1;
     private JButton pinkButton;
     private JButton blackButton;
@@ -27,23 +28,27 @@ public class Settings extends JFrame implements ActionListener,FileReadWrite{
     private JLabel Tcolor;
     private JButton greenButton;
     private JButton redButton;
-    private JButton blackButton1;
-    private JButton whiteButton1;
+    private JButton bbText;
+    private JButton wbText;
     private JLabel fileType;
 
     //String[] comboOptions = {"file1", "file2", "file3"};
-
+    TextBasedWindow TBchanges = new TextBasedWindow();
     ImageIcon foxImage = new ImageIcon("src/main/resources/fox.png"); // lade till en icon till våra fönster
 
     public Settings(){// konstruktor
         initializer();// method som innehåller saker vi vill ha till våra fönster
-        setSize(350,450); // specifikt för detta fönster
+        setSize(400,460); // specifikt för detta fönster
 
         goBack.addActionListener(this); //istället för detta går de att använda en lambda expprestion(e -> "de som ska göras")
+        confirmButton.addActionListener(this);
         pinkButton.addActionListener(this);
         blackButton.addActionListener(this);
         whiteButton.addActionListener(this);
-
+        redButton.addActionListener(this);
+        bbText.addActionListener(this);
+        wbText.addActionListener(this);
+        greenButton.addActionListener(this);
 
         textsize.addChangeListener(new ChangeListener() {
             @Override
@@ -65,51 +70,63 @@ public class Settings extends JFrame implements ActionListener,FileReadWrite{
             Splashpage backToSplash = new Splashpage(); // öppnar splachpage
             dispose();//stänger ner Settingsfönstret
         }
+        if(e.getSource().equals(confirmButton)){
+            TBchanges = new TextBasedWindow();
+        }
         if(e.getSource().equals(pinkButton)){
             setColor(e);
-        } else if (e.getSource().equals(blackButton)) {
+        }else if (e.getSource().equals(blackButton)) {
             setColor(e);
-        } else if (e.getSource().equals(whiteButton)) {
+        }else if (e.getSource().equals(whiteButton)) {
+            setColor(e);
+        }else if (e.getSource().equals(redButton)) {
+            setColor(e);
+        }else if (e.getSource().equals(greenButton)) {
+            setColor(e);
+        }else if (e.getSource().equals(wbText)) {
+            setColor(e);
+        }else if (e.getSource().equals(bbText)) {
             setColor(e);
         }
     }
     public void setColor(ActionEvent colorSelect){// tänkte testa ändra bagrundsfärg med knaptryck, något jag kanske utvecklar senare
 
         if(colorSelect.getSource().equals(pinkButton)){
-            mainPanel.setBackground(Color.pink);
-            topp.setBackground(Color.pink);
-            center.setBackground(Color.pink);
-            bottom.setBackground(Color.pink);
+            TBchanges.setPanel1(Color.pink);
+            TBchanges.setBTextArea(Color.pink);
         }
-        if(colorSelect.getSource().equals(blackButton)){
-            mainPanel.setBackground(Color.BLACK);
-            topp.setBackground(Color.BLACK);
-            center.setBackground(Color.BLACK);
-            bottom.setBackground(Color.BLACK);
+        if(colorSelect.getSource().equals(blackButton) || colorSelect.getSource().equals(bbText)){
+            TBchanges.setPanel1(Color.black);
+            TBchanges.setBTextArea(Color.black);
+            if(colorSelect.getSource().equals(bbText)){
+                TBchanges.setFTextArea(Color.black);
+            }
         }
-        if(colorSelect.getSource().equals(whiteButton)){
-            mainPanel.setBackground(Color.white);
-            topp.setBackground(Color.white);
-            center.setBackground(Color.white);
-            bottom.setBackground(Color.white);
+        if(colorSelect.getSource().equals(whiteButton) || colorSelect.getSource().equals(wbText)){
+            TBchanges.setPanel1(Color.white);
+            TBchanges.setBTextArea(Color.white);
+            if(colorSelect.getSource().equals(wbText)){
+                TBchanges.setFTextArea(Color.white);
+            }
+        }
+        if(colorSelect.getSource().equals(greenButton)){
+            TBchanges.setFTextArea(Color.green);
+        }
+        if(colorSelect.getSource().equals(redButton)){
+            TBchanges.setFTextArea(Color.red);
         }
 
     }
-
-
     private void initializer() {// basic fakta för fönster
         setContentPane(mainPanel);
         setIconImage(foxImage.getImage()); // lägger till iconen till fönstret
         setTitle("I am working");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
         setVisible(true);
 
     }
-
     public String ReadConfig(String filename, String value){
         //ReadConfig("src/main/resources/Config", "BackgroundcolorOptions")//struktur för att kalla informationen
         return FileRead(filename, value);
     }
-
 }
