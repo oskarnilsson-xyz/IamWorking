577f92ac55bf5fecf4495aa6ef2cf0c46b98c7c1
oskarnilsson-xyz <114058419+oskarnilsson-xyz@users.noreply.github.com>
2022-12-04 16:08:10 +0100

    Feat:
    Surviel Window, + a bunch of files, sorry!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,10 @@
 package org.example;
-import javax.swing.*;
+
 public class Main {
     public static String configPath = "src/main/resources/Config";
     public static void main(String[] args) {
+        var Surviel = new SurveilWindow();
+
 
         //var TextBasedWindow = new TextBasedWindow();
         var manager = new Manager();
